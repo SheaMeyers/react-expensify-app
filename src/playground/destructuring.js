a48284ff4d51ed96cd5baa70253a47167b018d66
SheaMeyers <shea.meyers@gmail.com>
2019-09-29 19:45:19 +0200

    Continue working with destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,16 +1,24 @@
-const person = {
-    name: 'Shea',
-    age: 28,
-    location: {
-        city: 'Voorburg',
-        temp: 18
-    }
-}
+// Object Destructuring
 
-const { name: firstName = 'Anonymous', age } = person;
+// const person = {
+//     name: 'Shea',
+//     age: 28,
+//     location: {
+//         city: 'Voorburg',
+//         temp: 18
+//     }
+// }
 
-console.log(`${firstName} is ${age}.`)
+// const { name: firstName = 'Anonymous', age } = person;
 
-const { city, temp: tempurature } = person.location;
+// console.log(`${firstName} is ${age}.`)
 
-console.log(`It's ${tempurature} in ${city}.`)
+// const { city, temp: tempurature } = person.location;
+
+// console.log(`It's ${tempurature} in ${city}.`)
+
+const address = ['1299 S Juniper Street', 'Philadephia', 'Pennsylvania', '19147'];
+
+const [, city, state = 'New York'] = address;
+
+console.log(`You are in ${city} ${state}`);
