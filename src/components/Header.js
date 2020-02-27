cd261eafd5553cc309dcc90bef49a54d1b0307c9
Shea Meyers <shea.meyers@gmail.com>
2020-02-27 06:25:52 +0100

    Add styling for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    <header className="header">
+        <div className="content-container">
+            <Link className="header__title" to="/" exact={true}>
+                <h1>Expensify</h1>
+            </Link>
+        </div>
     </header>
 );
 
